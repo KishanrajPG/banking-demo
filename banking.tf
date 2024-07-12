fddf9c19b3d1a9bca69598a93a8304ad49fbcaa2
KishanrajPG <69359891+KishanrajPG@users.noreply.github.com>
2024-07-12 16:43:48 +0530

    Update banking.tf

--- banking.tf
+++ banking.tf
@@ -126,19 +126,19 @@ resource "aws_eip" "proj-eip" {
 
 # Creating an ubuntu EC2 instance
 resource "aws_instance" "Prod-Server" {
- ami = "ami-0ef82eeba2c7a0eeb"
- instance_type = "t2.micro"
- availability_zone = "us-east-1a"
- key_name = "bankingkeypair"
- network_interface {
- device_index = 0
- network_interface_id = aws_network_interface.proj-ni.id
- }
- user_data  = <<-EOF
- #!/bin/bash
-     sudo apt-get update -y
- EOF
- tags = {
- Name = "Prod-Server"
- }
+  ami = "ami-0ef82eeba2c7a0eeb"
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1b"  # Use the correct availability zone here
+  key_name = "bankingkeypair"
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.proj-ni.id
+  }
+  user_data  = <<-EOF
+    #!/bin/bash
+    sudo apt-get update -y
+  EOF
+  tags = {
+    Name = "Prod-Server"
+  }
 }
